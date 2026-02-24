c105885a907f3ee903a7bbc1639602a1d8c200fa
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-02-24 17:06:40 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+sendTelegram("🚪 <b>LOGOUT</b>\n👤 Username: {$_SESSION['username']}\n🌐 IP: {$_SERVER['REMOTE_ADDR']}");
 session_destroy();
 header('Location: index.php');
+exit;
 ?>
