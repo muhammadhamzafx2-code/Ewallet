567a7ff170bdeafef6cc3149c83b5edaf62f693b
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-06-11 13:35:29 +0100

    Update deposit_success.php

--- deposit_success.php
+++ deposit_success.php
@@ -7,7 +7,7 @@
 </head>
 <body class="success-page">
     <div class="success-container">
-        <div class="success-icon">✅</div>
+        <div class="success-icon">❌️</div>
         <h1>Deposit Successful!</h1>
         <p>Deposit Pending,Transaction will be Refunded within 24hours<br>having issue with your bank,pls contact your bank</p>
         <a href="dashboard.php" class="btn-success">Go to Dashboard</a>
